83a762ba303841bdcab9394d9d95ed577194b811
JeffreyWang <jlwang3@illinois.edu>
2020-03-04 22:20:03 -0600

    Initialize and Organize String parameter. Covert String to Upper Case. Create an array of entries.

--- src/tictactoe.cc
+++ src/tictactoe.cc
@@ -9,8 +9,20 @@
 namespace tictactoe {
 
 using std::string;
+const static int boardLength = 9;
 
 TicTacToeState EvaluateBoard(const string& board) {
+
+    //https://stackoverflow.com/questions/735204/convert-a-string-in-c-to-upper-case
+    std::string = std::transform(board.begin(), board.end(), board.begin(), ::toupper);
+
+    std::string BOARD [boardLength] =
+
+    for (int n = 0; n < boardLength; n++) {
+        std::string BOARD [n] = string.sub
+    }
+
+
   return TicTacToeState::NoWinner;
 }
 
